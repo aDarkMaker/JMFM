6584784c9d0e4a0dd576b339d15d1ed05a00fe96
aDarkMaker <2261265112@qq.com>
2026-08-22 20:44:01 +0800

    chore: scaffold react native typescript project

---
+++ index.ts
@@ -0,0 +1,9 @@
+/**
+ * Entry point of the app.
+ * Registers the root component with the native runtime.
+ */
+import {AppRegistry} from 'react-native';
+import App from './src/app/App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
